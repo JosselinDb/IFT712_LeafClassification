7bbf915fc0af34c8c627e321049e342d57e970a4
JosselinDb <dubois.josselin2000@gmail.com>
2022-03-27 18:45:36 +0200

    knn implementation

--- classification/classifiers/knn.py
+++ classification/classifiers/knn.py
@@ -1,5 +1,18 @@
+import numpy as np
 from classification.classifier import Classifier
-
+from sklearn.neighbors import KNeighborsClassifier
 
 class KNN(Classifier):
-    pass
+    """
+    Classifier with K-Nearest-Neighbors method
+
+    Parameters
+        k: int > 0
+            number of neighbors to consider
+        **kwargs:
+            keyword arguments for the sklearn classifier
+    """
+    def __init__(self, k: int=10, **kwargs) -> None:
+        kwargs['n_neighbors'] = k
+
+        super().__init__(KNeighborsClassifier, [], kwargs)
