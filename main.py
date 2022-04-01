a3bb10d133ea9e96988128223dc2e97e64114091
JosselinDb <dubois.josselin2000@gmail.com>
2022-04-01 02:00:24 +0200

    reshaping folders

--- main.py
+++ main.py
@@ -4,5 +4,19 @@
 # JOURDIN Yann (jouy0701)
 # --- 
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+from classification.classifiers.knn import KNN
+from classification.classifiers.multi_layer import MultiLayerPerceptron
+from classification.classifiers.random_forest import RandomForest
+from classification.classifiers.svm import SVM
+from data_management import DataManager
+
+
+
 if __name__ == "__main__":
-    pass
+    dm = DataManager()
+    dm.make_validation_set()
+    
+    
